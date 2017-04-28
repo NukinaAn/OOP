9e0ee321b3453bd27eb96269683c99dc1d59a515
NukinaAn <nukina2694@gmail.com>
2017-04-28 18:00:34 +0300

    Update Student.java

--- src/main/java/uits/jv1614/oop1/Student.java
+++ src/main/java/uits/jv1614/oop1/Student.java
@@ -1,59 +1,110 @@
-package uits.jv1614.oop1;
+package uits.jv1614.oop;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.*;
-import java.text.*;
-import java.util.Calendar;
 
 
 public class Student {
+   private int id;
+   private String SurName;
+   private String Name;
+   private String MiddleName;
+  // private Date birthday;
+   private String address;
+   private int number;
+   private int Faculty;
+   private int course;
+   private int Group;
+    
  
-    int id;
-    String SurName;
-    String Name;
-    String MiddleName;
-    Date birthday;
-    String adress;
-    int number;
-    int Faculty;
-    int course;
-    int Group;
-   
-    
-   public Student() throws ParseException{
-      id = 1;        
-      Name = "Василий"; 
-      SurName = "Васильев";
-      MiddleName = "Васильевич";
-      adress = "Барабашова 32" ;
-      number = 3369012;
-      Faculty = 3;
-      course = 3;
-      Group = 334;
+    public Student(int id, String Name, String SurName, String MiddleName, String address, int number,int Faculty,int course, int Group) {
+        this.id = id;
+        this.Name = Name;
+        this.SurName = SurName;
+        this.MiddleName = MiddleName;
+        this.address = address;
+        this.number = number;
+        this.Faculty = Faculty;
+        this.course = course;
+        this.Group = Group;     
     }
-
-    public Student(int id){
-        
-      this.Name = "Петр"; 
-      this.SurName = "Петренко";
-      this.MiddleName = "Петрович";
-      this.adress = "Героев Труда 88" ;
-      this.number = 3365249;
-      this.Faculty = 3;
-      this.course = 3;
-      this.Group = 334;
-       
+    public int getid() {
+        return id;
+    }
+ 
+    public void setid(int id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return Name;
+    }
+ 
+    public void setName(String name) {
+        this.Name = name;
+    }
+ 
+    public String getSurname() {
+        return SurName;
+    }
+ 
+    public void setSurname(String surname) {
+        this.SurName = surname;
+    }
+    public String getMiddleName() {
+        return MiddleName;
+    }
+ 
+    public void setMiddleName(String surname) {
+        this.MiddleName = MiddleName;
+    }
+ 
+    public String getAddress() {
+        return address;
+    }
+ 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getnumber() {
+        return number;
+    }
+ 
+    public void setnumber(int number) {
+        this.number = number;
+    }
+    public int getFaculty() {
+        return Faculty;
+    }
+ 
+    public void setFaculty(int Faculty) {
+        this.Faculty = Faculty;
+    }
+     public int getcourse() {
+        return course;
+    }
+ 
+    public void setcourse(int course) {
+        this.course = course;
+    }
+    public int getGroup() {
+        return Group;
+    }
+ 
+    public void setGroup(int Group) {
+        this.Group = Group;
     }
     
-    public Student(int id, int number){
-        
-      this.Name = "Олег"; 
-      this.SurName = "Дурнев";
-      this.MiddleName = "Анатольевич";
-      this.adress = "Блюхера 21" ;
-      this.Faculty = 4;
-      this.course = 2;
-      this.Group = 425;
-     }
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id:'" + id + '\'' +
+                ", name='" + Name + '\'' +
+                ", surname='" + SurName + '\'' +
+                ", MiddleName='" + MiddleName + '\'' +
+                ", address='" + address + '\'' +
+                ", number='" + number + '\'' +
+                ", Faculty='" + Faculty + '\'' +
+                ", course='" + course + '\'' +
+                ", Group='" + Group + '\'' +
+                '}';
+    }
 }
