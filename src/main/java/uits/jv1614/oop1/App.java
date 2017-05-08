0c960575eb2a85d30853c98bb462d4ba7be57d7f
NukinaAn <nukina2694@gmail.com>
2017-05-08 17:24:44 +0300

    Update App.java

--- src/main/java/uits/jv1614/oop1/App.java
+++ src/main/java/uits/jv1614/oop1/App.java
@@ -1,31 +1,41 @@
 package uits.jv1614.oop;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+
 
 public class App {
-     public static void main(String[] args) throws ParseException {
-    
-         Student stud = new Student (1, "Петр" , "Петров", "Петрович",  "Барабашова 32", 3356423, 4, 4, 443);
-            System.out.println(stud);
-          
-         Student stud1 = new Student();
-            System.out.println(stud1);
+     public static void main(String[] args) {
          
-         Student stud3 = new Student (3, "Иван" , "Иванов", "Иванович",  "Советская 67", 3364398, 3, 2, 323);
-            System.out.println(stud3);
-            
-         Student stud4 = new Student (4, "Евгений" , "Жмуркин", "Андреевич",  "Герое Труда 7", 3367498, 3, 2, 323);
-            System.out.println(stud4);
-         
-         Student stud5 = new Student (5, "Николай" , "Кукушкин", "Адольфович",  "Блюхера 13", 3364398, 6, 4, 645);
-            System.out.println(stud5);
-            
-         Student stud6 = new Student (6, "Василий" , "Мышкин", "Олегович",  "Академика Павлова 9", 3364398, 6, 4, 645);
-            System.out.println(stud6);
      }
-   }
+         static void studens() throws ParseException{
+             
+         Student[] stud = new Student[6];
+         stud [0] = new Student(1, "Петр" , "Петров", "Петрович", new Date(1995, 9, 9),  "Барабашова 32", 3356423, 4, 4, 443);
+         stud [1] = new Student(2, "Артур" , "Герасимов", "Герасимович", new Date(1995, 8, 10), "Холодная гора 4", 3326579, 4, 4, 443);
+         stud [2] = new Student(3, "Иван" , "Иванов", "Иванович", new Date(1997, 12, 9), "Советская 67", 3364398, 3, 2, 323);
+         stud [3] = new Student(4, "Евгений" , "Жмуркин", "Андреевич", new Date(1997, 5, 2), "Герое Труда 7", 3367498, 3, 2, 323);
+         stud [4] = new Student(5, "Николай" , "Кукушкин", "Адольфович", new Date(1995, 9, 9), "Блюхера 13", 3364398, 6, 4, 645);
+         stud [5] = new Student(6, "Василий" , "Мышкин", "Олегович", new Date(1995, 3, 18), "Академика Павлова 9", 3364398, 6, 4, 645);
+         
+         Date p = new Date(1990, 9, 9);
+         System.out.println();
+         
+        for (int i = 0; i <= 5; i++) {
+            stud[i].Faculty();
+        }
+        System.out.println();
+        for (int i = 0; i <= 5; i++) {
+            stud[i].FacCourse();
+        }
+        System.out.println();
+        for (int i = 0; i <= 5; i++) {
+        stud[i].date_birthday(p);
+        }
+        System.out.println();
+        for (int i = 0; i <= 5; i++) {
+            stud[i].Group();
+        }
+        
+     }
+}
